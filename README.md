9313d3e5623c3747a4cb52ce64aa2e013c12e01a
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-11-29 05:56:14 +0100

    initialize monorepo

---
+++ README.md
@@ -0,0 +1,55 @@
+# Dystore
+
+![GitHub Actions](https://github.com/dystcz/dystore/actions/workflows/tests.yaml/badge.svg)
+
+> [!CAUTION]
+> This package is currently under heavy development. You should proceed with caution.
+
+Collection of packages built on top of [Lunar](https://github.com/lunarphp/lunar).
+
+## Requirements
+
+-   PHP ^8.2
+-   Laravel 11
+-   [Lunar requirements](https://docs.lunarphp.io/core/installation.html#server-requirements)
+
+## Documentation
+
+-   Coming soon
+
+### Testing
+
+```bash
+composer test
+```
+
+### Packages
+
+-   Coming soon
+
+### Modules
+
+-   Coming soon
+
+### Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information what has changed recently.
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) for details.
+
+### Security
+
+If you discover any security related issues, please email dev@dy.st instead of using the issue tracker.
+
+## Credits
+
+-   [All Contributors](../../contributors)
+-   [Lunar](https://github.com/lunarphp/lunar) for providing awesome e-commerce package
+-   [Laravel JSON:API](https://github.com/laravel-json-api/laravel)
+    which is a brilliant JSON:API layer for Laravel applications
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ monorepo-builder.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
+
+return static function (MBConfig $mbConfig): void {
+
+    $mbConfig->workers([
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        AddTagToChangelogReleaseWorker::class,
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+        PushNextDevReleaseWorker::class,
+    ]);
+
+    $mbConfig->packageDirectories([
+        __DIR__.'/packages',
+        __DIR__.'/modules',
+    ]);
+
+    $mbConfig->packageDirectoriesExcludes([
+        __DIR__.'/packages/paypal',
+        __DIR__.'/packages/rewards',
+    ]);
+
+    $mbConfig->dataToAppend([
+        ComposerJsonSection::REQUIRE_DEV => [
+            'barryvdh/laravel-ide-helper' => '^3.0',
+            'larastan/larastan' => '^2.0',
+            'laravel-json-api/testing' => '^3.0',
+            'laravel/pint' => '^1.7',
+            'orchestra/testbench' => '^9.0',
+            'pestphp/pest' => '^2.0',
+            'pestphp/pest-plugin-faker' => '^2.0',
+            'pestphp/pest-plugin-laravel' => '^2.0',
+            'spatie/laravel-ray' => '^1.32',
+            'symplify/monorepo-builder' => '^11.2',
+        ],
+    ]);
+
+};
