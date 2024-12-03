aaf2a8c8af9af3b5bd21b906a7bf04cd82c284aa
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-12-03 15:59:16 +0100

    disable marking pending payment when order already has one of future statuses

--- src/Domain/Payments/Actions/MarkPendingPayment.php
+++ src/Domain/Payments/Actions/MarkPendingPayment.php
@@ -13,6 +13,18 @@ class MarkPendingPayment
      */
     public function __invoke(Order $order): Order
     {
+        $disabledStatuses = [
+            'payment-received',
+            'dispatched',
+            'delivered',
+            'cancelled',
+            'on-hold',
+        ];
+
+        if (in_array($order->status, $disabledStatuses)) {
+            return $order;
+        }
+
         $order = (new ChangeOrderStatus)($order, OrderStatus::PENDING_PAYMENT);
 
         return $order;
