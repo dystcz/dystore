dd39d970e320e2867d4ae27371aadd7b116f89af
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-12-09 23:02:08 +0100

    update docblock

--- packages/api/src/Domain/Products/Models/Product.php
+++ packages/api/src/Domain/Products/Models/Product.php
@@ -2,13 +2,12 @@
 
 namespace Dystore\Api\Domain\Products\Models;
 
-use Dystore\Api\Domain\Products\Builders\ProductBuilder;
 use Dystore\Api\Domain\Products\Concerns\InteractsWithDystoreApi;
 use Dystore\Api\Domain\Products\Contracts\Product as ProductContract;
 use Lunar\Models\Product as LunarProduct;
 
 /**
- * @method static ProductBuilder query()
+ * @method static \Dystore\Api\Domain\Products\Builders\ProductBuilder query()
  */
 class Product extends LunarProduct implements ProductContract
 {
