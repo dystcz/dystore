c61575dc0e9c4ac552a00b0f14e81c2e0ee2c9ea
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-01-14 12:50:11 +0100

    test that meta can be updated

--- tests/api/Feature/Domain/Orders/JsonApi/V1/UpdateOrderTest.php
+++ tests/api/Feature/Domain/Orders/JsonApi/V1/UpdateOrderTest.php
@@ -5,7 +5,8 @@ use Dystore\Api\Domain\Users\Models\User;
 use Dystore\Tests\Api\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(TestCase::class, RefreshDatabase::class);
+uses(TestCase::class, RefreshDatabase::class)
+    ->group('orders');
 
 it('can update order by its owner', function () {
     /** @var TestCase $this */
@@ -28,10 +29,42 @@ it('can update order by its owner', function () {
         ->jsonApi()
         ->expects('orders')
         ->withData($data)
-        ->patch('http://localhost/api/v1/orders/'.$order->getRouteKey());
+        ->patch(serverUrl('/orders/').$order->getRouteKey());
 
     $response->assertFetchedOne($order);
-})->group('orders');
+});
+
+it('can update order meta', function () {
+    /** @var TestCase $this */
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    /** @var Order $order */
+    $order = Order::factory()->for($user)->create();
+
+    $data = [
+        'type' => 'orders',
+        'id' => (string) $order->getRouteKey(),
+        'attributes' => [
+            'meta' => [
+                'packeta_id' => 123456789,
+            ],
+        ],
+    ];
+
+    $response = $this
+        ->jsonApi()
+        ->expects('orders')
+        ->withData($data)
+        ->patch(serverUrl('/orders/').$order->getRouteKey());
+
+    $response->assertFetchedOne($order);
+
+    $this->assertDatabaseHas((new Order)->getTable(), [
+        'meta' => json_encode(['packeta_id' => 123456789]),
+    ]);
+});
 
 it('cannot update order by other user', function () {
     /** @var TestCase $this */
@@ -54,11 +87,11 @@ it('cannot update order by other user', function () {
         ->jsonApi()
         ->expects('orders')
         ->withData($data)
-        ->patch('http://localhost/api/v1/orders/'.$order->getRouteKey());
+        ->patch(serverUrl('/orders/').$order->getRouteKey());
 
     $response->assertErrorStatus([
         'detail' => 'This action is unauthorized.',
         'status' => '403',
         'title' => 'Forbidden',
     ]);
-})->group('orders');
+});
