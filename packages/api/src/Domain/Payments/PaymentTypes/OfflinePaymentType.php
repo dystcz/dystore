1c9a8637e484cd60da79c7785af72aed499c2685
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-01-16 06:37:29 +0100

    rename method to fail

--- packages/api/src/Domain/Payments/PaymentTypes/OfflinePaymentType.php
+++ packages/api/src/Domain/Payments/PaymentTypes/OfflinePaymentType.php
@@ -33,7 +33,7 @@ class OfflinePaymentType extends AbstractPayment
             try {
                 $this->order = $this->cart->createOrder();
             } catch (DisallowMultipleCartOrdersException|CartException $e) {
-                return $this->failedAuthorization($e->getMessage(), $paymentType);
+                return $this->fail($e->getMessage(), $paymentType);
             }
         }
 
@@ -63,7 +63,7 @@ class OfflinePaymentType extends AbstractPayment
         return $authorization;
     }
 
-    public function failedAuthorization(
+    public function fail(
         string $message = 'Failed to authorize payment',
         string $paymentType = 'offline',
     ): PaymentAuthorize {
