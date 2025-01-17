8429b96eef4cdd4a48f4c94373d6bccc505bbbb1
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-01-17 18:59:32 +0100

    add product id

--- packages/api/src/Domain/ProductVariants/JsonApi/V1/ProductVariantSchema.php
+++ packages/api/src/Domain/ProductVariants/JsonApi/V1/ProductVariantSchema.php
@@ -93,6 +93,7 @@ class ProductVariantSchema extends Schema
             Str::make('mpn'),
             Str::make('ean'),
             Str::make('tax_ref'),
+            Str::make('product_id'),
 
             Map::make('availability', [
                 ArrayHash::make('stock')
