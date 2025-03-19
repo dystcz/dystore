692cb5f172f8a3a3e4add33b3d95ef053cf27226
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-20 00:02:07 +0100

    update price builder
    * add inCurrency
    * rename baseOrAll to inCustomerGroups

--- packages/api/src/Domain/Prices/Builders/PriceBuilder.php
+++ packages/api/src/Domain/Prices/Builders/PriceBuilder.php
@@ -2,26 +2,56 @@
 
 namespace Dystore\Api\Domain\Prices\Builders;
 
+use Dystore\Api\Domain\Storefront\Managers\StorefrontSessionManager;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Facades\App;
 use Lunar\Base\StorefrontSessionInterface;
 use Lunar\Models\Contracts\CustomerGroup as CustomerGroupContract;
 
 /**
+ * @param  StorefrontSessionManager  $storefront
+ *
  * @extends Builder<Model>
  */
 class PriceBuilder extends Builder
 {
-    public function baseOrAll(): self
+    private StorefrontSessionInterface $storefront;
+
+    public function __construct(QueryBuilder $query)
     {
+        $this->storefront = App::make(StorefrontSessionInterface::class);
+
+        parent::__construct($query);
+    }
+
+    public function inCurrency(): self
+    {
+        return $this->where(
+            'currency_id',
+            $this->storefront->getCurrency()->id
+        );
+    }
+
+    public function inCustomerGroups(): self
+    {
+        $customerGroups = App::make(StorefrontSessionInterface::class)->getCustomerGroups();
+
         return $this->when(
-            value: fn () => App::make(StorefrontSessionInterface::class)
-                ->getCustomerGroups()
-                ->filter(fn (CustomerGroupContract $group) => ! $group->default)
-                ->isEmpty(),
-            callback: fn (self $query) => $query->base(),
-            default: fn (self $query) => $query,
+            value: fn () => $this->storefront->getCustomerGroups()
+                ->filter(fn (?CustomerGroupContract $group = null) => $group)
+                ->isNotEmpty(),
+            callback: fn (self $query) => $query
+                ->whereIn(
+                    'customer_group_id',
+                    $customerGroups->pluck('id')->toArray()
+                )
+                ->orWhere(
+                    'customer_group_id',
+                    null
+                ),
+            default: fn (self $query) => $query->base(),
         );
     }
 
