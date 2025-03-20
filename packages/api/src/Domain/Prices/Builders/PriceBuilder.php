d389bd9a6187a861d6ab85126f3cfb5d4b778dc9
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-20 02:14:00 +0100

    add static builder methods

--- packages/api/src/Domain/Prices/Builders/PriceBuilder.php
+++ packages/api/src/Domain/Prices/Builders/PriceBuilder.php
@@ -2,30 +2,87 @@
 
 namespace Dystore\Api\Domain\Prices\Builders;
 
-use Dystore\Api\Domain\Storefront\Managers\StorefrontSessionManager;
+use Illuminate\Contracts\Database\Query\Builder as BuilderContract;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Facades\App;
 use Lunar\Base\StorefrontSessionInterface;
+use Lunar\Models\Contracts\CustomerGroup as CustomerGroupContract;
+use Lunar\Models\Price;
 
 /**
- * @param  StorefrontSessionManager  $storefront
- *
- * @method PriceBuilder inCurrency(?string $column, ?string $table)
- * @method PriceBuilder inCustomerGroups(?string $column, ?string $table)
- * @method PriceBuilder basePrices(?string $table)
- *
  * @extends Builder<Model>
  */
 class PriceBuilder extends Builder
 {
-    private StorefrontSessionInterface $storefront;
-
-    public function __construct(QueryBuilder $query)
+    public static function getStorefrontSession(): StorefrontSessionInterface
     {
-        $this->storefront = App::make(StorefrontSessionInterface::class);
+        return App::make(StorefrontSessionInterface::class);
+    }
 
-        parent::__construct($query);
+    public static function scopeCurrency(BuilderContract $query, string $column = 'currency_id', ?string $table = null): void
+    {
+        $table = $table ?? (new Price)->getModel()->getTable();
+
+        /** @var PriceBuilder $query */
+        $query->where(
+            "{$table}.{$column}",
+            static::getStorefrontSession()->getCurrency()->getKey()
+        );
+    }
+
+    public function inCurrency(): self
+    {
+        static::scopeCurrency($this);
+
+        return $this;
+    }
+
+    public static function scopeCustomerGroups(BuilderContract $query, string $column = 'customer_group_id', ?string $table = null): void
+    {
+        $customerGroups = static::getStorefrontSession()->getCustomerGroups();
+        $table = $table ?? (new Price)->getModel()->getTable();
+
+        /** @var PriceBuilder $query */
+        $query->when(
+            value: fn () => $customerGroups
+                ->filter(fn (?CustomerGroupContract $group = null) => $group)
+                ->isNotEmpty(),
+            callback: fn (BuilderContract $query) => $query
+                ->where(fn (BuilderContract $query) => $query
+                    ->whereIn(
+                        "{$table}.customer_group_id",
+                        $customerGroups->pluck('id')->toArray()
+                    )
+                    ->orWhere(
+                        "{$table}.customer_group_id",
+                        null
+                    )
+                ),
+            default: fn (BuilderContract $query) => static::scopeBasePrices($query, $table),
+        );
+    }
+
+    public function inCustomerGroups(): self
+    {
+        static::scopeCustomerGroups($this);
+
+        return $this;
+    }
+
+    public static function scopeBasePrices(BuilderContract $query, ?string $table = null): void
+    {
+        $table = $table ?? (new Price)->getModel()->getTable();
+
+        $query
+            ->where("{$table}.min_quantity", 1)
+            ->where("{$table}.customer_group_id", null);
+    }
+
+    public function base(): self
+    {
+        static::scopeBasePrices($this);
+
+        return $this;
     }
 }
