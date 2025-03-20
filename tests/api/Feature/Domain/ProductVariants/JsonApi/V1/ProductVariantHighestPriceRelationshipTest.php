4e0eed7502117d4dc37b7232daa744a893803694
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-20 06:10:17 +0100

    add customer group highest and lowest price tests for product variants

--- tests/api/Feature/Domain/ProductVariants/JsonApi/V1/ProductVariantHighestPriceRelationshipTest.php
+++ tests/api/Feature/Domain/ProductVariants/JsonApi/V1/ProductVariantHighestPriceRelationshipTest.php
@@ -4,8 +4,13 @@ use Dystore\Api\Domain\Products\Models\Product;
 use Dystore\Api\Domain\ProductVariants\Factories\ProductVariantFactory;
 use Dystore\Tests\Api\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
+use Lunar\Base\StorefrontSessionInterface;
+use Lunar\Models\CustomerGroup;
 
-uses(TestCase::class, RefreshDatabase::class);
+uses(TestCase::class, RefreshDatabase::class)
+    ->group('product_variants');
 
 it('can read highest price through relationship', function () {
     /** @var TestCase $this */
@@ -25,4 +30,55 @@ it('can read highest price through relationship', function () {
         ->assertSuccessful()
         ->assertFetchedOne($variant->prices->sortByDesc('price')->first())
         ->assertDoesntHaveIncluded();
-})->group('product_variants');
+});
+
+it('can read correct highest price when customer group is set', function () {
+    /** @var TestCase $this */
+    $variant = ProductVariantFactory::new()
+        ->for(Product::factory(), 'product')
+        ->withPrice()
+        ->withPrice()
+        ->withPrice()
+        ->create();
+
+    $highestPrice = $variant->prices->sortByDesc(fn ($price) => $price->price->value)->first();
+
+    $customerGroup = CustomerGroup::factory()
+        ->create();
+
+    $highestPrice->update([
+        'customer_group_id' => $customerGroup->getKey(),
+    ]);
+
+    $highestBasePrice = $variant->prices
+        ->filter(fn ($price) => $price->getKey() !== $highestPrice->getKey())
+        ->sortByDesc(fn ($price) => $price->price->value)
+        ->first();
+
+    $response = $this
+        ->jsonApi()
+        ->expects('prices')
+        ->get(serverUrl("/product_variants/{$variant->getRouteKey()}/highest_price"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($highestBasePrice)
+        ->assertDoesntHaveIncluded();
+
+    App::make(StorefrontSessionInterface::class)
+        ->setCustomerGroups(Collection::make([$customerGroup]));
+
+    $response = $this
+        ->jsonApi()
+        ->expects('prices')
+        ->get(serverUrl("/product_variants/{$variant->getRouteKey()}/highest_price"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($highestPrice)
+        ->assertDoesntHaveIncluded();
+});
+
+afterEach(function () {
+    App::make(StorefrontSessionInterface::class)->forget();
+});
--- tests/api/Feature/Domain/ProductVariants/JsonApi/V1/ProductVariantLowestPriceRelationshipTest.php
+++ tests/api/Feature/Domain/ProductVariants/JsonApi/V1/ProductVariantLowestPriceRelationshipTest.php
@@ -4,8 +4,13 @@ use Dystore\Api\Domain\Products\Models\Product;
 use Dystore\Api\Domain\ProductVariants\Factories\ProductVariantFactory;
 use Dystore\Tests\Api\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\App;
+use Lunar\Base\StorefrontSessionInterface;
+use Lunar\Models\CustomerGroup;
 
-uses(TestCase::class, RefreshDatabase::class);
+uses(TestCase::class, RefreshDatabase::class)
+    ->group('product_variants');
 
 it('can read lowest price through relationship', function () {
     /** @var TestCase $this */
@@ -25,4 +30,51 @@ it('can read lowest price through relationship', function () {
         ->assertSuccessful()
         ->assertFetchedOne($variant->prices->sortBy('price')->first())
         ->assertDoesntHaveIncluded();
-})->group('product_variants');
+});
+
+it('can read correct lowest price when customer group is set', function () {
+    /** @var TestCase $this */
+    $variant = ProductVariantFactory::new()
+        ->for(Product::factory(), 'product')
+        ->withPrice()
+        ->withPrice()
+        ->withPrice()
+        ->create();
+
+    $lowestPrice = $variant->prices->sortBy(fn ($price) => $price->price->value)->first();
+
+    $customerGroup = CustomerGroup::factory()
+        ->create();
+
+    $lowestPrice->update([
+        'customer_group_id' => $customerGroup->getKey(),
+    ]);
+
+    $lowestBasePrice = $variant->prices
+        ->filter(fn ($price) => $price->getKey() !== $lowestPrice->getKey())
+        ->sortBy(fn ($price) => $price->price->value)
+        ->first();
+
+    $response = $this
+        ->jsonApi()
+        ->expects('prices')
+        ->get(serverUrl("/product_variants/{$variant->getRouteKey()}/lowest_price"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($lowestBasePrice)
+        ->assertDoesntHaveIncluded();
+
+    App::make(StorefrontSessionInterface::class)
+        ->setCustomerGroups(Collection::make([$customerGroup]));
+
+    $response = $this
+        ->jsonApi()
+        ->expects('prices')
+        ->get(serverUrl("/product_variants/{$variant->getRouteKey()}/lowest_price"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($lowestPrice)
+        ->assertDoesntHaveIncluded();
+});
