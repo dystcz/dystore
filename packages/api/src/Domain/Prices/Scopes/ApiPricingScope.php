889819bf360bd4e66832adc0221a94f123bea6c9
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-20 01:05:10 +0100

    rename scope

--- packages/api/src/Domain/Prices/Scopes/ApiPricingScope.php
+++ packages/api/src/Domain/Prices/Scopes/ApiPricingScope.php
@@ -5,14 +5,21 @@ namespace Dystore\Api\Domain\Prices\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Lunar\Models\Price;
 
-class StorefrontApiScope implements Scope
+class ApiPricingScope implements Scope
 {
     public function apply(Builder $builder, Model $model): void
     {
         /** @var \Dystore\Api\Domain\Prices\Builders\PriceBuilder $builder */
         $builder
-            ->inCustomerGroups()
-            ->inCurrency();
+            ->inCurrency(
+                'currency_id',
+                (new Price)->getTable()
+            )
+            ->inCustomerGroups(
+                'customer_group_id',
+                (new Price)->getTable()
+            );
     }
 }
