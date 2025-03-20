98372a65ef2c61cbb8d9709d9d37d03b2a9ac278
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-20 01:05:31 +0100

    use macros

--- packages/api/src/Domain/Prices/Builders/PriceBuilder.php
+++ packages/api/src/Domain/Prices/Builders/PriceBuilder.php
@@ -8,10 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Facades\App;
 use Lunar\Base\StorefrontSessionInterface;
-use Lunar\Models\Contracts\CustomerGroup as CustomerGroupContract;
 
 /**
  * @param  StorefrontSessionManager  $storefront
+ *
+ * @method PriceBuilder inCurrency(?string $column, ?string $table)
+ * @method PriceBuilder inCustomerGroups(?string $column, ?string $table)
+ * @method PriceBuilder basePrices(?string $table)
  *
  * @extends Builder<Model>
  */
@@ -24,43 +27,5 @@ class PriceBuilder extends Builder
         $this->storefront = App::make(StorefrontSessionInterface::class);
 
         parent::__construct($query);
-    }
-
-    public function inCurrency(): self
-    {
-        return $this->where(
-            'currency_id',
-            $this->storefront->getCurrency()->id
-        );
-    }
-
-    public function inCustomerGroups(): self
-    {
-        $customerGroups = App::make(StorefrontSessionInterface::class)->getCustomerGroups();
-
-        return $this->when(
-            value: fn () => $this->storefront->getCustomerGroups()
-                ->filter(fn (?CustomerGroupContract $group = null) => $group)
-                ->isNotEmpty(),
-            callback: fn (self $query) => $query
-                ->whereIn(
-                    'customer_group_id',
-                    $customerGroups->pluck('id')->toArray()
-                )
-                ->orWhere(
-                    'customer_group_id',
-                    null
-                ),
-            default: fn (self $query) => $query->base(),
-        );
-    }
-
-    public function base(): self
-    {
-        $table = $this->getModel()->getTable();
-
-        return $this
-            ->where("{$table}.min_quantity", 1)
-            ->where("{$table}.customer_group_id", null);
     }
 }
