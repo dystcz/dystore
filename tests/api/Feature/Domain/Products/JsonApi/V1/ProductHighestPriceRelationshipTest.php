4e28eb78ca1600ab3caba331cbddce62d4391879
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-20 06:06:45 +0100

    add customer group highest price test

--- tests/api/Feature/Domain/Products/JsonApi/V1/ProductHighestPriceRelationshipTest.php
+++ tests/api/Feature/Domain/Products/JsonApi/V1/ProductHighestPriceRelationshipTest.php
@@ -29,6 +29,50 @@ it('can read highest price through relationship', function () {
         ->assertDoesntHaveIncluded();
 });
 
+it('can read correct highest price when customer group is set', function () {
+    /** @var TestCase $this */
+    $product = ProductFactory::new()
+        ->withPrices(3)
+        ->create();
+
+    $highestPrice = $product->prices->sortByDesc(fn ($price) => $price->price->value)->first();
+
+    $customerGroup = CustomerGroup::factory()
+        ->create();
+
+    $highestPrice->update([
+        'customer_group_id' => $customerGroup->getKey(),
+    ]);
+
+    $highestBasePrice = $product->prices
+        ->filter(fn ($price) => $price->getKey() !== $highestPrice->getKey())
+        ->sortByDesc(fn ($price) => $price->price->value)
+        ->first();
+
+    $response = $this
+        ->jsonApi()
+        ->expects('prices')
+        ->get(serverUrl("/products/{$product->getRouteKey()}/highest_price"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($highestBasePrice)
+        ->assertDoesntHaveIncluded();
+
+    App::make(StorefrontSessionInterface::class)
+        ->setCustomerGroups(Collection::make([$customerGroup]));
+
+    $response = $this
+        ->jsonApi()
+        ->expects('prices')
+        ->get(serverUrl("/products/{$product->getRouteKey()}/highest_price"));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedOne($highestPrice)
+        ->assertDoesntHaveIncluded();
+});
+
 it('can read highest price through relationship with includes', function (string $includePath, string $type, callable $getModel) {
     /** @var TestCase $this */
     $product = ProductFactory::new()
