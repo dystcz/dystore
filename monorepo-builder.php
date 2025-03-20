0339fb8c06809e4de11940562e1f7151c67725ae
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-03-20 06:35:15 +0100

    update laravel-json-api versions

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -35,7 +35,7 @@ return static function (MBConfig $mbConfig): void {
         ComposerJsonSection::REQUIRE_DEV => [
             'barryvdh/laravel-ide-helper' => '^3.0',
             'larastan/larastan' => '^3.0',
-            'laravel-json-api/testing' => '^3.0',
+            'laravel-json-api/testing' => '^3.1',
             'laravel/pint' => '^1.7',
             'orchestra/testbench' => '^9.0',
             'pestphp/pest' => '^2.0',
