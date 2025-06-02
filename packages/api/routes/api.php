145344c18d114088c7c6ac86a133f5b54519d883
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2025-06-03 01:19:00 +0200

    add webhook route to has routes concern

--- packages/api/routes/api.php
+++ packages/api/routes/api.php
@@ -1,11 +1,5 @@
 <?php
 
-use Dystore\Api\Domain\Payments\Contracts\HandlePaymentWebhookController;
 use Dystore\Api\Facades\Api;
-use Illuminate\Support\Facades\Route;
 
 Api::routes();
-
-// Payments
-Route::post('{paymentDriver}/webhook', HandlePaymentWebhookController::class)
-    ->name('payments.webhook');
