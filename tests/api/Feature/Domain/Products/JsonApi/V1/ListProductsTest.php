20ce90dfcf9f80ff2d40b370ce772ee5e5754e6d
Jakub Theimer <5587309+repl6669@users.noreply.github.com>
2025-07-14 13:51:07 +0200

    Fix: Scope products by customer groups (#65)
    * add customer group scope method docblock
    
    * scope products by customer groups from storefront sesh
    
    * add test
    
    ---------
    
    Co-authored-by: Jakub Theimer <5587309+theimerj@users.noreply.github.com>

--- tests/api/Feature/Domain/Products/JsonApi/V1/ListProductsTest.php
+++ tests/api/Feature/Domain/Products/JsonApi/V1/ListProductsTest.php
@@ -1,12 +1,15 @@
 <?php
 
+use Carbon\Carbon;
+use Dystore\Api\Domain\CustomerGroups\Models\CustomerGroup;
 use Dystore\Api\Domain\Prices\Models\Price;
 use Dystore\Api\Domain\Products\Models\Product;
 use Dystore\Api\Domain\ProductVariants\Models\ProductVariant;
 use Dystore\Tests\Api\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(TestCase::class, RefreshDatabase::class);
+uses(TestCase::class, RefreshDatabase::class)
+    ->group('products');
 
 it('can list bare products', function () {
     /** @var TestCase $this */
@@ -27,7 +30,49 @@ it('can list bare products', function () {
         ->assertSuccessful()
         ->assertFetchedMany($products)
         ->assertDoesntHaveIncluded();
-})->group('products');
+});
+
+it('can list only products in correct customer group', function () {
+    /** @var TestCase $this */
+    $defaultCustomerGroup = CustomerGroup::getDefault();
+
+    $exclusiveCustomerGroup = CustomerGroup::factory()->create([
+        'name' => 'Super ultra exclusive club',
+        'handle' => 'swag',
+    ]);
+
+    $products = Product::factory()
+        ->has(
+            ProductVariant::factory()->has(Price::factory())->count(2),
+            'variants'
+        )
+        ->count(3)
+        ->create();
+
+    $exclusiveProducts = Product::factory()
+        ->has(
+            ProductVariant::factory()->has(Price::factory())->count(2),
+            'variants'
+        )
+        ->count(3)
+        ->create();
+
+    foreach ($exclusiveProducts as $product) {
+        /** @var Product $product */
+        $product->unscheduleCustomerGroup($defaultCustomerGroup, ['visible' => false]);
+        $product->scheduleCustomerGroup($exclusiveCustomerGroup, Carbon::now());
+    }
+
+    $response = $this
+        ->jsonApi()
+        ->expects('products')
+        ->get(serverUrl('/products'));
+
+    $response
+        ->assertSuccessful()
+        ->assertFetchedMany($products)
+        ->assertDoesntHaveIncluded();
+});
 
 it('cannot list unpublished products', function () {
     /** @var TestCase $this */
