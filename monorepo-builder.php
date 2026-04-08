2a3c57afb044477cc25cb18271f8cfd8b28b15c1
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2026-04-08 22:02:48 +0200

    Drop Laravel 11 support, add Laravel 13 support

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -37,7 +37,7 @@ return static function (MBConfig $mbConfig): void {
             'larastan/larastan' => '^3.0',
             'laravel-json-api/testing' => '^3.1',
             'laravel/pint' => '^1.7',
-            'orchestra/testbench' => '^9.0|^10.0',
+            'orchestra/testbench' => '^10.0|^11.0',
             'pestphp/pest' => '^4.0',
             'pestphp/pest-plugin-faker' => '^4.0',
             'pestphp/pest-plugin-laravel' => '^4.0',
